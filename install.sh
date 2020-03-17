3030de3a3c86e0e67e5dd7c14dafb62272e3828b
Sir Cipherz <0jean9jean3@gmail.com>
2020-03-17 23:42:05 +0000

    Debbuging

--- install.sh
+++ install.sh
@@ -12,10 +12,13 @@ install -m 700 .config/wallpaper.jpg ~/.config/wallpaper.jpg
 
 # i3 and terminator
 install -m 700 .config/i3/config ~/.config/i3/config
-install -m 700
+install -m 700 .config/terminator/config ~/.config/terminator/config
 
 # polybar and stuff
-install -m 700 .config/polybar/* ~/.config/polybar/
+install -m 700 .config/polybar/config ~/.config/polybar/config
+install -m 700 .config/polybar/isrunning-openvpn.sh ~/.config/polybar/isrunning-openvpn.sh
+install -m 700 .config/polybar/launch.sh ~/.config/polybar/launch.sh
+install -m 700 .config/polybar/time.sh ~/.config/polybar/time.sh
 
 # the .*rc
 install -m 700 .vimrc ~/.vimrc
