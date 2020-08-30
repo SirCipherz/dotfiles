d8b859c1eaa0b6e61c4aa9fd28a4145aa40de772
SirCipherz <sircipherz@mail.com>
2020-08-30 18:26:07 +0200

    Removing a big mistake

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 # .config
 cp -r ~/.config/bspwm ./.config/     # bspwm config file
-cp -r ~/.wallpapers ./.wallpapers/   # Wallpaper
+cp -r ~/.wallpapers ./               # Wallpaper
 cp -r ~/.config/polybar/ ./.config/  # Polybar
 cp ~/.config/picom.conf ./.config/   # Compton
 cp -r ~/.config/sxhkd ./.config/     # sxhkd
