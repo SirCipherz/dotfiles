f36543ffc8d24f184cd3aae996529a8f3247b8dc
SirCipherz <sircipherz@mail.com>
2020-12-03 21:19:52 +0100

    polybar : smaller font, new emojis, adding wifi and sound, longer xwindow

--- .config/polybar/headphones.sh
+++ .config/polybar/headphones.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $(grep "Power-Map: 0x0" /proc/asound/card0/codec\#0) == "Power-Map: 0x01" ]]; then
-    echo "Unplugged"
+    echo ""
 else
-    echo "Plugged"
+    echo "    connected"
 fi
